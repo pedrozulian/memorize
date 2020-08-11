6f7891b58891b37ea72bfedfc05d15d9ba39c898
Pedro Zulian <pedrotzf@gmail.com>
2020-08-11 16:45:55 -0300

    imports e exports necessários

--- fazendo-feira/src/app/presentation/base/base.module.ts
+++ fazendo-feira/src/app/presentation/base/base.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* Module of Users */
+import { UsersModule } from './users/users.module'; 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    UsersModule,
+  ],
+  exports: [
+    UsersModule,
   ]
 })
 export class BaseModule { }
--- fazendo-feira/src/app/presentation/presentation.module.ts
+++ fazendo-feira/src/app/presentation/presentation.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+/* Module of Base */
+import { BaseModule } from './base/base.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BaseModule,
+  ],
+  exports: [
+    BaseModule,
   ]
 })
 export class PresentationModule { }
