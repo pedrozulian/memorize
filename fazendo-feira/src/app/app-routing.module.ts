9f5a58e69c284f2771d94b41bfacf21a8a523ae6
Pedro Zulian <pedrotzf@gmail.com>
2020-08-11 18:31:08 -0300

    definido primeira rota e router-outlet no html principal

--- fazendo-feira/src/app/app-routing.module.ts
+++ fazendo-feira/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './presentation/base/layout/components/header/header.component'; 
+
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
