91c7e9a19d8a24a80651a038b7e4cd026166ef70
Pedro Zulian <pedrotzf@gmail.com>
2020-08-14 18:49:10 -0300

    import MatDialog no modulo do material

--- fazendo-feira/src/app/material/material.module.ts
+++ fazendo-feira/src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const material = [
   CommonModule,
   MatToolbarModule,
   MatButtonModule,
+  MatDialogModule,
 ];
 
 @NgModule({
