d92e63c4493075367a0714d02aac29559efaff44
Pedro Zulian <pedrotzf@gmail.com>
2020-08-14 18:51:06 -0300

    chamando função p abrir dialog de sign-in

--- fazendo-feira/src/app/presentation/base/layout/components/header/header.component.ts
+++ fazendo-feira/src/app/presentation/base/layout/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SignInComponent } from '../../../dialog/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    this.dialog.open(SignInComponent);
   }
 
 }
