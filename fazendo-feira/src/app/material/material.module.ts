8a8535af9262653787cf49968ec5bc3ecd7a4147
Pedro Zulian <pedrotzf@gmail.com>
2020-08-15 14:19:11 -0300

    import matdivider

--- fazendo-feira/src/app/material/material.module.ts
+++ fazendo-feira/src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 
 const material = [
   CommonModule,
@@ -12,6 +13,7 @@ const material = [
   MatButtonModule,
   MatDialogModule,
   MatMenuModule,
+  MatDividerModule,
 ];
 
 @NgModule({
