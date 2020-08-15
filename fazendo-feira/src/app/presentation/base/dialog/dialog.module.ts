78d1fd0be39e566b6bf72fab580cf4d9d9895055
Pedro Zulian <pedrotzf@gmail.com>
2020-08-15 12:07:40 -0300

    novo componente "sign up" e suas devidas importações

--- fazendo-feira/src/app/presentation/base/dialog/dialog.module.ts
+++ fazendo-feira/src/app/presentation/base/dialog/dialog.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { MaterialModule } from '../../../material/material.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
     MaterialModule,
--- fazendo-feira/src/app/presentation/base/layout/layout.module.ts
+++ fazendo-feira/src/app/presentation/base/layout/layout.module.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './components/header/header.component';
 
 import { MaterialModule } from '../../../material/material.module';
 import { SignInComponent } from '../dialog/sign-in/sign-in.component';
+import { SignUpComponent } from '../dialog/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [HeaderComponent],
   entryComponents: [
     SignInComponent,
+    SignUpComponent,
   ],
   imports: [
     CommonModule,
