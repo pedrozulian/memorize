45cff86821187a7306a719534973506025c2ee1e
Pedro Zulian <pedrotzf@gmail.com>
2020-08-15 12:10:13 -0300

    import dialog e matMenu do material design

--- fazendo-feira/src/app/material/material.module.ts
+++ fazendo-feira/src/app/material/material.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
 
 const material = [
   CommonModule,
   MatToolbarModule,
   MatButtonModule,
+  MatDialogModule,
+  MatMenuModule,
 ];
 
 @NgModule({
