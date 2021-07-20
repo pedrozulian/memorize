2ffe5537dd61b641a43d1317dbe04dd67e939db6
Pedro <pedrotzf@gmail.com>
2021-07-19 21:47:15 -0300

    feat(App.tsx): importando header no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Header></Header>
   );
 }
 
