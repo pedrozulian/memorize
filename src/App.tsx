af5b671502384e3e17345a3220bcd0d1ed0fb19f
Pedro <pedrotzf@gmail.com>
2021-07-20 21:13:44 -0300

    feat(App.tsx): costumizando o tema dos componentes
    definindo cores

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,29 @@ import {
 
 import './App.css';
 import Home from './pages/Home';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { lightBlue, pink } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: lightBlue,
+    secondary: pink,
+  }
+});
+
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path="/" component={Home}></Route>
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={Home}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
     );
 }
 
