43fef21e7254bc526872da7416bb60232f640bf9
Pedro <pedrotzf@gmail.com>
2021-07-22 21:35:58 -0300

    feat(SignUp.tsx): component de cadastro praticamente pronto

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import undrawYoungHappy from '../../assets/drawkit/men_typing_notebook.svg';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            '& .MuiTextField-root': {
+                marginBottom: theme.spacing(1),
+                width: '70%'
+            },
+            
+        },
+
+        btnCreate: {
+            backgroundcolor: '#ffeb3b'
+        }
+    })
+);
+
 function SignUpComponent() {
+    const classes = useStyles();
+
     return(
-        <div>SignUp</div>
+        <Grid container xs={12}>
+            <Grid item xs={12} sm={8}>
+                <form className={classes.root}>
+                    <TextField required id="" label="Nome"></TextField>
+                    <TextField required id="" label="E-mail" type="email"></TextField>
+                    <TextField required id="" label="Senha" type="password"></TextField>
+                    <TextField required id="" label="Confirmar senha" type="password"></TextField>
+                    <Box mt={2} justifyItems="center">
+                        <Button variant="contained" color="primary" className="">CADASTRAR</Button>
+                    </Box>
+                </form>
+            </Grid>
+            <Grid item sm={4}>
+                <img src={undrawYoungHappy} alt="" />
+            </Grid>
+        </Grid>        
     );
 }
 
