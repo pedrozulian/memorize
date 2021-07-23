3bee9c336aec94de15deb67f655325ecb4cf0449
Pedro <pedrotzf@gmail.com>
2021-07-22 21:35:31 -0300

    refactor(Session.tsx): algumas alterações de css

--- src/components/session/Session.tsx
+++ src/components/session/Session.tsx
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 import SwipeableViews from 'react-swipeable-views';
-import { ThemeProvider } from "@material-ui/core";
-import { useTheme } from '@material-ui/core/styles';
+import { useTheme, makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { a11yProps, TabPanelComponent } from "./TabPanel";
 import SignInComponent from "../signIn/SignIn";
 import SignUpComponent from "../signUp/SignUp";
 
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        wrapper: {
+            backgroundColor: '#ffffff'
+        }
+    })
+);
+
 function Session() {
     const theme = useTheme();
+    const classes = useStyles();
     const [value, setValue] = useState(0);
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
@@ -22,12 +31,12 @@ function Session() {
     }
 
     return(
-        <div>
+        <Box boxShadow={3} className={classes.wrapper}>
             <Tabs
             value={value}
             onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
+            indicatorColor="secondary"
+            textColor="secondary"
             >
                 <Tab label="Inscrever-se" {...a11yProps(0)}></Tab>
                 <Tab label="Login" {...a11yProps(1)}></Tab>
@@ -44,7 +53,7 @@ function Session() {
                     <SignInComponent></SignInComponent>
                 </TabPanelComponent>
             </SwipeableViews>
-        </div>
+        </Box>
     );
 }
 
